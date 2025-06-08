1f1b119e901e34d9f99e0ba7cf5a23c870175bf6
AlexandreBihi <104452389+AlexandreBihi@users.noreply.github.com>
2025-06-08 15:45:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -353,7 +353,7 @@ with tabs[2]:
     st.markdown("---")
     st.markdown("### ☁️ WordCloud by Review Tone")
 
-    mots_exclus = {"none", "louis", "vuitton"}
+    mots_exclus = {"none", "louis", "vuitton", "store", "Paris", "came", "come"}
 
     text_pos = ' '.join(
         word for line in df_filtered[df_filtered['stars'] >= 4]['cleaned_words_all'].dropna()
